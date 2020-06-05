e38aac471c833ed4afb5ee1a4eedf6933c267311
Quinn Hanson <quinnhanson55@gmail.com>
2020-06-05 16:17:39 -0700

    remove warnings on console

--- src/App.js
+++ src/App.js
@@ -1,58 +1,42 @@
-import React from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import Sidebar from './components/Sidebar';
-import Appbar from './components/Appbar';
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import Sidebar from "./components/Sidebar";
+import Appbar from "./components/Appbar";
 import Login from "./components/Login";
 
 const loginPage = () => {
-    return (<div className="App">
-        <Login />
+  return (
+    <div className="App">
+      <Login />
     </div>
-    );
-}
+  );
+};
 
 let contentPage = () => {
-    return (<div className="App">
-            <Appbar/>
-            <Sidebar />
-        </div>
-    );
-}
+  return (
+    <div className="App">
+      <Appbar />
+      <Sidebar />
+    </div>
+  );
+};
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  render() {
+    if (this.props.isLoggedIn) {
+      return contentPage();
+    } else {
+      return loginPage();
     }
-
-    render() {
-        if (this.props.isLoggedIn) {
-            return contentPage();
-        } else {
-            return loginPage();
-
-        }
-    }
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.isLoggedIn,
-        username: state.username
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.isLoggedIn,
+    username: state.username,
+  };
+};
 
 export default connect(mapStateToProps)(App);
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/*<Appbar />*/}
-//       {/*<Sidebar />*/}
-//       <Login />
-//     </div>
-//   );
-// }
-//
-// export default App;
--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,27 +1,24 @@
-import contentType from "../../data/ContentTypeEnum";
-
 export const LOG_IN = "LOG_IN";
 export const LOG_OUT = "LOG_OUT";
 export const USERNAME = "USERNAME";
 
-
 export const toggleSidebar = () => ({
-    type: 'TOGGLE_SIDEBAR'
-})
+  type: "TOGGLE_SIDEBAR",
+});
 
 export const logIn = () => ({
-    type: LOG_IN
-})
+  type: LOG_IN,
+});
 
 export const logOut = () => ({
-    type: LOG_OUT
-})
+  type: LOG_OUT,
+});
 
-export const usernameSubmit = payload => ({
-    type: USERNAME,
-    username: payload
-})
+export const usernameSubmit = (payload) => ({
+  type: USERNAME,
+  username: payload,
+});
 
-export const selectContentPage = payload => ({
-    type: payload
-})
+export const selectContentPage = (payload) => ({
+  type: payload,
+});
