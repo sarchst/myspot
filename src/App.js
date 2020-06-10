ff1dff76a69962edf1b1aac547122bcbe066cb18
Sarchen Starke <sarchenstarke@gmail.com>
2020-06-10 11:21:02 -0700

    Implemented spotify login and now playing feature for logged in user

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import Sidebar from "./components/Sidebar";
 import Appbar from "./components/Appbar";
 import Login from "./components/Login";
+import SpotifyWebApi from "spotify-web-api-js";
+const spotifyApi = new SpotifyWebApi();
 
 const loginPage = () => {
   return (
@@ -23,12 +25,62 @@ let contentPage = () => {
 };
 
 class App extends React.Component {
-  render() {
-    if (this.props.isLoggedIn) {
-      return contentPage();
-    } else {
-      return loginPage();
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
     }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: {
+        name: "Not Checked",
+        albumArt: "",
+      },
+    };
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url,
+        },
+      });
+    });
+  }
+  render() {
+    // if (this.props.isLoggedIn) {
+    //   return contentPage();
+    // } else {
+    //   return loginPage();
+    // }
+    return (
+      <div className="App">
+        <a href="http://localhost:8888">
+          <button>Login</button>
+        </a>
+        <div>Now playing {this.state.nowPlaying.name}</div>
+        <div>
+          <img
+            src={this.state.nowPlaying.albumArt}
+            style={{ width: 100 }}
+          ></img>
+        </div>
+        <button onClick={() => this.getNowPlaying()}>Check nwo playing</button>
+      </div>
+    );
   }
 }
 
