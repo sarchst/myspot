de614251c115c3296e5073780c91c280571f5397
Austin <wjaustinlee@gmail.com>
2020-07-08 22:27:43 -0700

    Revert "added concurrently script to start all servers at once"
    This reverts commit 0293532d

--- server/db/index.js
+++ server/db/index.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 var dbname = "myspot";
-var username;
-var uri = `mongodb+srv://myspot:myspot@myspot.gu485.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-
-// MONGOUSER=436pr0ject
+var uri = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPW}@myspot.gu485.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, { useNewUrlParser: true }).catch((e) => {
   console.error("Connection error", e.message);
