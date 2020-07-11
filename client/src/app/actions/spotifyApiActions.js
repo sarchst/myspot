6c0b221daeb3b093996cfea41bafae3f98f7f63f
Austin <wjaustinlee@gmail.com>
2020-07-11 05:15:21 -0700

    implemented user POST at login; disabled settings required in  user-model

--- client/src/app/actions/spotifyApiActions.js
+++ client/src/app/actions/spotifyApiActions.js
@@ -1,6 +1,32 @@
 export const SPOTIFY_USER_ME = "SPOTIFY_USER_ME";
 
-export const submitSpotifyApiUserMe = (SpotifyApiUserInfoMe) => ({
+export const submitSpotifyApiUserMe = (SpotifyApiUserInfoMe) => {
+  return (dispatch) => {
+    // save to mongodb via mongoose.prototype.save()
+    let mongoUserObject = {
+      _id: SpotifyApiUserInfoMe.id,
+      username: SpotifyApiUserInfoMe.display_name,
+      email: SpotifyApiUserInfoMe.email,
+      followers: [],
+      following: [],
+      posts: [],
+    };
+    fetch(`http://localhost:9000/user`, {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(mongoUserObject),
+    })
+      .then((response) => {
+        console.log(response);
+        dispatch(submitSpotifyApiUserMeThunk(SpotifyApiUserInfoMe));
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+const submitSpotifyApiUserMeThunk = (SpotifyApiUserInfoMe) => ({
   type: SPOTIFY_USER_ME,
   payload: SpotifyApiUserInfoMe,
 });
