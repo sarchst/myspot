321df91e005734791446c65aaf1477d0213a5694
Quinn Hanson <quinnhanson55@gmail.com>
2020-07-23 12:34:32 -0700

    made deployment changes x3

--- server/app.js
+++ server/app.js
@@ -124,7 +124,7 @@ app.get("/callback", function (req, res) {
 
         // we can also pass the token to the browser to make requests from there
         res.redirect(
-          "http://localhost:3000/#" +
+          "https://myspotmusic.herokuapp.com/#" +
             querystring.stringify({
               access_token: access_token,
               refresh_token: refresh_token,
