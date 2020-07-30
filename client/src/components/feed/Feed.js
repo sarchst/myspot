837a61d630af4319632329fb1e05ec8388fcdbf7
unknown <melouie@students.cs.ubc.ca>
2020-07-30 09:05:17 -0700

    kinda working but component state not resetting between page navs

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -5,17 +5,49 @@ import Post from "./Post";
 import MakePost from "./MakePost";
 import { toggleLike, fetchFeed } from "../../app/actions/feedActions";
 import { fetchPosts } from "../../app/actions/postActions";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Typography } from "@material-ui/core";
+import Emoji from "react-emoji-render";
 
 class Feed extends React.Component {
   state = {
     userProfilePics: {},
+    items: [],
+    hasMore: true,
   };
 
   componentDidMount() {
     this.props.fetchFeed(this.props.user.id);
   }
 
- 
+  componentDidUpdate(prevProps) {
+    console.log("CDU");
+    // Typical usage (don't forget to compare props):
+    if (this.props.feed.posts !== prevProps.feed.posts) {
+      console.log("CDU if");
+      this.setState({
+        items: this.props.feed.posts.slice(0, 5),
+      });
+    }
+    console.log("length", this.state.items.length);
+    console.log("items", this.state.items);
+  }
+
+  fetchMoreData = () => {
+    if (this.state.items.length >= this.props.feed.posts.length) {
+      this.setState({ hasMore: false });
+      return;
+    }
+    let n = this.state.items.length;
+    // a fake async api call like which sends
+    // 5 more records in 0.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(this.props.feed.posts.slice(n, n + 5)),
+      });
+    }, 500);
+  };
+
   render() {
     const { toggleLike, user } = this.props;
 
@@ -24,14 +56,30 @@ class Feed extends React.Component {
         <div>
           <MakePost />
           <div>
-            {this.props.feed.posts.map((p) => (
-              <Post
-                key={p._id}
-                postdata={p}
-                toggleLike={() => toggleLike(p, user.id )}
-                userId={user.id}
-              />
-            ))}
+            <InfiniteScroll
+              dataLength={this.state.items.length}
+              next={this.fetchMoreData}
+              hasMore={this.state.hasMore}
+              loader={
+                <Typography color="primary" style={{ textAlign: "center" }}>
+                  <Emoji text="Loading... :eyes:" />
+                </Typography>
+              }
+              endMessage={
+                <Typography color="primary" style={{ textAlign: "center" }}>
+                  <Emoji text="Yay! You have seen it all :party_popper:" />
+                </Typography>
+              }
+            >
+              {this.state.items.map((p) => (
+                <Post
+                  key={p._id}
+                  postdata={p}
+                  toggleLike={() => toggleLike(p, user.id)}
+                  userId={user.id}
+                />
+              ))}
+            </InfiniteScroll>
           </div>
         </div>
       );
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -10,6 +10,9 @@ import ProfileTable from "./ProfileTable";
 import { fetchUserSettings } from "../../app/actions/settingsActions";
 import { fetchProfilePic } from "../../app/actions/imageUploadActions";
 import DeletePostDialog from "../feed/DeletePostDialog";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Typography } from "@material-ui/core";
+import Emoji from "react-emoji-render";
 
 const styles = (theme) => ({
   root: {
@@ -29,10 +32,45 @@ const styles = (theme) => ({
 });
 
 class Profile extends React.Component {
+  state = {
+    items: [],
+    hasMore: true,
+    count: 0
+  };
+
   componentDidMount = () => {
     this.props.fetchPosts(this.props.user.id);
     this.props.fetchUserSettings(this.props.user.id);
     this.props.fetchProfilePic(this.props.user.id);
+  };
+
+  componentDidUpdate(prevProps) {
+    console.log("PROFILE: CDU");
+    // Typical usage (don't forget to compare props):
+    if (this.props.posts !== prevProps.posts) {
+      console.log("PROFILE: CDU if");
+      this.setState({
+        items: this.props.posts.slice(0, 5),
+      });
+    }
+    console.log("count", this.state.count);
+    console.log("length", this.state.items.length);
+    console.log("items", this.state.items);
+  }
+
+  fetchMoreData = () => {
+    if (this.state.items.length >= this.props.posts.length) {
+      this.setState({ hasMore: false });
+      return;
+    }
+    let n = this.state.items.length;
+    // a fake async api call like which sends
+    // 5 more records in 0.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(this.props.posts.slice(n, n + 5)),
+      });
+    }, 500);
   };
 
   render() {
@@ -43,22 +81,38 @@ class Profile extends React.Component {
         <ProfileCard />
         <ProfileTable />
         <MakePost />
-        <div>
-          {this.props.posts && this.props.posts.length ? (
-            // ALSO this could be a Feed component potentially with a is profile feed prop or something, not front burner issue though
-            // this.props.posts.map((p) => (
-            this.props.posts.map((p) => (
-              <Post
-                key={p._id}
-                postdata={p}
-                toggleLike={() => toggleLike(p, user.id)}
-                userId={user.id}
-              />
-            ))
-          ) : (
-            <h3 color="primary">Hmm..no posts yet. You should make one!</h3>
-          )}
-        </div>
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMore}
+          loader={
+            <Typography color="primary" style={{ textAlign: "center" }}>
+              <Emoji text="Loading... :eyes:"/>
+            </Typography>
+          }
+          endMessage={
+            <Typography color="primary" style={{ textAlign: "center" }}>
+              <Emoji text="Yay! You have seen it all :party_popper:" />
+            </Typography>
+          }
+        >
+          <div>
+            {this.props.posts && this.props.posts.length ? (
+              // ALSO this could be a Feed component potentially with a is profile feed prop or something, not front burner issue though
+              // this.props.posts.map((p) => (
+              this.props.posts.map((p) => (
+                <Post
+                  key={p._id}
+                  postdata={p}
+                  toggleLike={() => toggleLike(p, user.id)}
+                  userId={user.id}
+                />
+              ))
+            ) : (
+              <h3 color="primary">Hmm..no posts yet. You should make one!</h3>
+            )}
+          </div>
+        </InfiniteScroll>
       </div>
     );
   }
