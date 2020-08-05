ce861c38e2fd5f372c8a8f9d93e9735d6074ce57
Quinn Hanson <quinnhanson55@gmail.com>
2020-08-04 19:02:27 -0700

    fixed same bug for toggle like and delete comment

--- client/src/app/actions/postActions.js
+++ client/src/app/actions/postActions.js
@@ -174,7 +174,7 @@ export const addComment = (comment, profileFeedFilter, feedFilter) => {
 };
 
 export const deleteComment = (id, body, profileFeedFilter, feedFilter) => {
-  let authorId = body.authorId;
+  let userId = body.authorId;
   let commentInfo = body.commentInfo;
   return (dispatch) => {
     return axios
@@ -183,10 +183,10 @@ export const deleteComment = (id, body, profileFeedFilter, feedFilter) => {
         commentInfo
       )
       .then(() => {
-        dispatch(fetchPostsWithFilter(authorId, profileFeedFilter));
+        dispatch(fetchPostsWithFilter(id, profileFeedFilter));
       })
       .then(() => {
-        dispatch(fetchFeedWithFilter(authorId, feedFilter));
+        dispatch(fetchFeedWithFilter(userId, feedFilter));
       })
       .catch((error) => {
         throw error;
@@ -223,7 +223,7 @@ export const toggleLike = (post, id, profileFeedFilter, feedFilter) => {
         postInfo
       )
       .then(() => {
-        dispatch(fetchPostsWithFilter(id, profileFeedFilter));
+        dispatch(fetchPostsWithFilter(post.authorId, profileFeedFilter));
       })
       .then(() => {
         dispatch(fetchFeedWithFilter(id, feedFilter));
